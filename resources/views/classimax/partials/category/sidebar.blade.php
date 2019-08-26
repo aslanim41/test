dd5e0e2592043f60c479e0474838e9f071458d7a
Ümit EYİGÜN <aslan41@gmail.com>
2019-08-26 16:25:17 +0300

    test

---
+++ resources/views/classimax/partials/category/sidebar.blade.php
@@ -0,0 +1,72 @@
+<div class="category-sidebar">
+    <div class="widget category-list">
+        <h4 class="widget-header">All Category</h4>
+        <ul class="category-list">
+            <li><a href="#">Laptops <span>93</span></a></li>
+            <li><a href="#">Iphone <span>233</span></a></li>
+            <li><a href="#">Microsoft <span>183</span></a></li>
+            <li><a href="#">Monitors <span>343</span></a></li>
+        </ul>
+    </div>
+
+    <div class="widget category-list">
+        <h4 class="widget-header">Nearby</h4>
+        <ul class="category-list">
+            <li><a href="#">New York <span>93</span></a></li>
+            <li><a href="#">New Jersy <span>233</span></a></li>
+            <li><a href="#">Florida <span>183</span></a></li>
+            <li><a href="#">California <span>120</span></a></li>
+            <li><a href="#">Texas <span>40</span></a></li>
+            <li><a href="#">Alaska <span>81</span></a></li>
+        </ul>
+    </div>
+
+    <div class="widget filter">
+        <h4 class="widget-header">Show Produts</h4>
+        <select>
+            <option>Popularity</option>
+            <option value="1">Top rated</option>
+            <option value="2">Lowest Price</option>
+            <option value="4">Highest Price</option>
+        </select>
+    </div>
+
+    <div class="widget price-range">
+        <h4 class="widget-header">Price Range</h4>
+        <div class="block">
+            <b>$10</b>
+            <input id="ex2" type="text" class="span2" value="" data-slider-min="10"
+                   data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]"/>
+            <b>$5000</b>
+        </div>
+    </div>
+
+    <div class="widget product-shorting">
+        <h4 class="widget-header">By Condition</h4>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" value="">
+                Brand New
+            </label>
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" value="">
+                Almost New
+            </label>
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" value="">
+                Gently New
+            </label>
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" value="">
+                Havely New
+            </label>
+        </div>
+    </div>
+
+</div>
